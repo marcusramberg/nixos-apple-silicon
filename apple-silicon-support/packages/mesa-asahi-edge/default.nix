5ae3895af57dc02b88ab577571544d6aecf97e0c
Marcus Ramberg <marcus@means.no>
2025-01-07 20:03:00 +0100

    mesa-asahi-edge: update vendor

--- apple-silicon-support/packages/mesa-asahi-edge/default.nix
+++ apple-silicon-support/packages/mesa-asahi-edge/default.nix
@@ -1,31 +1,42 @@
-{ lib
-, fetchFromGitLab
-, pkgs
-, meson
-, llvmPackages
+{
+  lib,
+  fetchFromGitLab,
+  pkgs,
+  meson,
+  llvmPackages,
 }:
 
 # don't bother to provide Darwin deps
-((pkgs.callPackage ./vendor { OpenGL = null; Xplugin = null; }).override {
-  galliumDrivers = [ "swrast" "asahi" ];
-  vulkanDrivers = [ "swrast" "asahi" ];
-  enableGalliumNine = false;
-  # libclc and other OpenCL components are needed for geometry shader support on Apple Silicon
-  enableOpenCL = true;
-}).overrideAttrs (oldAttrs: {
-  # version must be the same length (i.e. no unstable or date)
-  # so that system.replaceRuntimeDependencies can work
-  version = "25.0.0";
-  src = fetchFromGitLab {
-    # tracking: https://pagure.io/fedora-asahi/mesa/commits/asahi
-    domain = "gitlab.freedesktop.org";
-    owner = "asahi";
-    repo = "mesa";
-    rev = "asahi-20241211";
-    hash = "sha256-Ny4M/tkraVLhUK5y6Wt7md1QBtqQqPDUv+aY4MpNA6Y=";
-  };
+(
+  (pkgs.callPackage ./vendor {
+  }).override
+  {
+    galliumDrivers = [
+      "softpipe"
+      "llvmpipe"
+      "asahi"
+    ];
+    vulkanDrivers = [
+      "swrast"
+      "asahi"
+    ];
+    # libclc and other OpenCL components are needed for geometry shader support on Apple Silicon
+  }
+).overrideAttrs
+  (oldAttrs: {
+    # version must be the same length (i.e. no unstable or date)
+    # so that system.replaceRuntimeDependencies can work
+    version = "25.0.0";
+    src = fetchFromGitLab {
+      # tracking: https://pagure.io/fedora-asahi/mesa/commits/asahi
+      domain = "gitlab.freedesktop.org";
+      owner = "asahi";
+      repo = "mesa";
+      rev = "asahi-20241211";
+      hash = "sha256-Ny4M/tkraVLhUK5y6Wt7md1QBtqQqPDUv+aY4MpNA6Y=";
+    };
 
-  mesonFlags = oldAttrs.mesonFlags ++ [
+    mesonFlags = oldAttrs.mesonFlags ++ [
       # we do not build any graphics drivers these features can be enabled for
       "-Dgallium-va=disabled"
       "-Dgallium-vdpau=disabled"
@@ -38,9 +49,9 @@
       "-Dlmsensors=disabled"
     ];
 
-  # replace patches with ones tweaked slightly to apply to this version
-  patches = [
-    ./disk_cache-include-dri-driver-path-in-cache-key.patch
-    ./opencl.patch
-  ];
-})
+    # replace patches with ones tweaked slightly to apply to this version
+    patches = [
+      ./disk_cache-include-dri-driver-path-in-cache-key.patch
+      ./opencl.patch
+    ];
+  })
